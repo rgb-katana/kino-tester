642ff9993edb5638c4ddeae13c1443f5774de643
rgb-katana <komarova1610@yandex.ru>
2022-09-18 18:22:31 +0300

    start working on start button and implement first question

--- script.js
+++ script.js
@@ -4,22 +4,61 @@
 const beginBtn = document.querySelector('.begin');
 const refuseBtn = document.querySelector('.refuse');
 const shareBtn = document.querySelector('.share');
+
 const mainContent = document.querySelector('.content');
 const textBlock = document.querySelector('.text-block');
 const testWindow = document.querySelector('.test-window');
+
 const backToMenu = document.querySelector('.back-to-menu');
 const copyLink = document.querySelector('.copy-link');
 
-shareBtn.addEventListener('click', function () {
+function hideMenuAndText() {
   textBlock.classList.add('hidden');
   testWindow.classList.add('hidden');
+}
+
+function showMenuAndText() {
+  textBlock.classList.remove('hidden');
+  testWindow.classList.remove('hidden');
+}
+
+function testingPhase() {
+  let slide = 1;
+  const backBtn = document.querySelector('.back');
+  const frwdBtn = document.querySelector('.forward');
+  while (slide < 10) {
+    const currentSlide = document.querySelector(`.question${slide}`);
+    currentSlide.classList.remove('hidden');
+    backBtn.addEventListener('click', function () {
+      if (slide != 1) {
+        slide -= 1;
+      } else if (slide == 1) {
+        currentSlide.classList.add('hidden');
+        showMenuAndText();
+      }
+    });
+    frwdBtn.addEventListener('click', function () {
+      if (slide != 10) {
+        console.log(`we here`);
+        slide += 1;
+        console.log(slide);
+      } else if (slide == 10) {
+        // win
+      }
+    });
+    slide += 1;
+    break;
+  }
+}
+
+shareBtn.addEventListener('click', function () {
+  hideMenuAndText();
   document.querySelector('.share-window').classList.remove('hidden');
 });
 
 backToMenu.addEventListener('click', function () {
   document.querySelector('.share-window').classList.add('hidden');
-  textBlock.classList.remove('hidden');
-  testWindow.classList.remove('hidden');
+  showMenuAndText();
 });
 
 copyLink.addEventListener('click', function () {
@@ -41,6 +80,10 @@ document
   .querySelector('.back-to-menu-gif')
   .addEventListener('click', function () {
     document.querySelector('.refuse-window').classList.add('hidden');
-    textBlock.classList.remove('hidden');
-    testWindow.classList.remove('hidden');
+    showMenuAndText();
   });
+
+beginBtn.addEventListener('click', function () {
+  hideMenuAndText();
+  testingPhase();
+});
